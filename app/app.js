6a90a9a2790979434d3db483cd09c328ff3fdd9e
Sam Decrock <sam.decrock@gmail.com>
2014-03-26 10:51:42 +0100

    disabled logging, fixed indentation

--- app/app.js
+++ app/app.js
@@ -10,6 +10,8 @@ app.use(express.static(__dirname + '/public'));
 var server = http.createServer(app);
 
 var io = require('socket.io').listen(server);
+io.set('log level', 0);
+
 var colors=["#0000FF","#00FF00","#00FFFF","#FF00FF","#FFFF00"];
 var players=[0,1,2,3,4];
 var connections=0;
@@ -17,8 +19,8 @@ var host=null;
 var clients=[];
 io.sockets.on("connection",function(socket){
 
-	console.log("socket");
-	console.log(socket);
+	console.log("socket connected");
+	// console.log(socket);
 	socket.emit('news', { hello: 'world' });
 	socket.on("client",function(data){
 		if(connections>colors.length){
@@ -41,43 +43,44 @@ io.sockets.on("connection",function(socket){
 	socket.on("host",function(){
 		host=socket;
 	});
-  	socket.on('up', function(data){
-  		if(host!=null) host.emit('up',{player:socket.playernr});
-  	});
-  	socket.on('down', function(data){
-  		if(host!=null) host.emit('down',{player:socket.playernr});
-  	});
-  	socket.on('shoot', function(data){
-  		if(host!=null) host.emit('shoot',{player:socket.playernr});
-  	});
-  	socket.on('kill',function(data){
-  		var counter=0;
-  		for(var i=0;i<5;i++){
-  			if(clients[i] && clients[i].playernr==data.player){
-  				clients[i].emit('dead',{score:data.score});
-  				break;
-  			}
-  		}
-  	});
-  	function disconnector(socket){
-  		socket.disconnect();
-  		clients.splice(clients.indexOf(socket),1);
-  	}
-  	socket.on("disconnect",function(data){
-  		if(socket.clientcolor){
-  			colors.push(socket.clientcolor);
-  			players.push(socket.playernr);
-  			clients.splice(clients.indexOf(socket),1);
-  			delete socket.clientcolor;
 
-  		}
-  		if(socket==host){
-  			host=null;
-  		}else{
-  		connections--;
+	socket.on('up', function(data){
+		if(host!=null) host.emit('up',{player:socket.playernr});
+	});
+	socket.on('down', function(data){
+		if(host!=null) host.emit('down',{player:socket.playernr});
+	});
+	socket.on('shoot', function(data){
+		if(host!=null) host.emit('shoot',{player:socket.playernr});
+	});
+	socket.on('kill',function(data){
+		var counter=0;
+		for(var i=0;i<5;i++){
+			if(clients[i] && clients[i].playernr==data.player){
+				clients[i].emit('dead',{score:data.score});
+				break;
+			}
+		}
+	});
+	function disconnector(socket){
+		socket.disconnect();
+		clients.splice(clients.indexOf(socket),1);
+	}
+	socket.on("disconnect",function(data){
+		if(socket.clientcolor){
+			colors.push(socket.clientcolor);
+			players.push(socket.playernr);
+			clients.splice(clients.indexOf(socket),1);
+			delete socket.clientcolor;
 
-  		}
-  	});
+		}
+		if(socket==host){
+			host=null;
+		}else{
+		connections--;
+
+		}
+	});
 });
 
 server.listen(port, function (err) {
